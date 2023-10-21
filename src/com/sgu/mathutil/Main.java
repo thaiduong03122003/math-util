feecf1b717f9a0542aa6dabf2b1ba341e4e95010
thaiduong03122003 <duongthai3122003@gmail.com>
2023-10-21 21:31:12 +0700

    Cap nhat lan cuoi

--- src/com/sgu/mathutil/Main.java
+++ src/com/sgu/mathutil/Main.java
@@ -5,6 +5,10 @@ public class Main {
         long resutl = MathUtil.getFactorial(5);
         System.out.println("5! = " + resutl);
         System.out.println("6! = 720; actual: " + MathUtil.getFactorial(6));
-        System.out.println("0! = 1;   actual: " + MathUtil.getFactorial(0)) ;
+        System.out.println("0! = 1;   actual: " + MathUtil.getFactorial(0));
+
+        //Thêm code sau khi đã upload lên github
+        System.out.println("1! = 1;   actual: " + MathUtil.getFactorial(1));
+        System.out.println("12! = " + MathUtil.getFactorial(12));
     }
 }
